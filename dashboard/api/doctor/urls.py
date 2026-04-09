2e78e442d73f201dab985944db82afb6c811e94c
Amir <amirmawla27@gmail.com>
2026-04-09 21:50:28 +0200

    feat: add allow doctor to update, get his details

--- dashboard/api/doctor/urls.py
+++ dashboard/api/doctor/urls.py
@@ -1,11 +1,15 @@
 from dashboard.api.doctor.views import (
     get_doctor_by_id,
     update_doctor_by_id,
+    get_logged_in_doctor,
+    update_logged_in_doctor,
 )
 from django.urls import path
 
 
 urlpatterns = [
+    path('me', get_logged_in_doctor, name='dashboard.api.doctor.get_logged_in_doctor'),
+    path('me/update', update_logged_in_doctor, name='dashboard.api.doctor.update_logged_in_doctor'),
     path('<int:id>', get_doctor_by_id, name='dashboard.api.doctor.get_doctor_by_id'),
     path('update/<int:id>', update_doctor_by_id, name='dashboard.api.doctor.update_doctor_by_id'),
 
--- dashboard/api/doctor/views.py
+++ dashboard/api/doctor/views.py
@@ -5,10 +5,16 @@ from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 
 from accounts.models import DoctorProfile
-from appointments.models import WeeklySchedule, ScheduleException
 from dashboard.api.doctor.serializers import (
     DoctorProfileModelSerializer
 )
+
+
+def _get_logged_in_doctor(request):
+    try:
+        return request.user.doctor_profile
+    except Exception:
+        return None
 
 
 @api_view(['GET'])
@@ -21,6 +27,28 @@ def get_doctor_by_id(request, id):
 @api_view(['PUT'])
 def update_doctor_by_id(request, id):
     doctor = get_object_or_404(DoctorProfile, pk=id)
+    doctor = DoctorProfileModelSerializer(doctor, data=request.data, partial=True)
+    if doctor.is_valid():
+        doctor.save()
+        return Response({'message': 'object updated!', 'doctor': doctor.data}, status=status.HTTP_200_OK)
+    return Response({'message': 'not valid', 'errors': doctor.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def get_logged_in_doctor(request):
+    doctor = _get_logged_in_doctor(request)
+    if not doctor:
+        return Response({'message': 'not allowed'}, status=status.HTTP_403_FORBIDDEN)
+    doctor = DoctorProfileModelSerializer(doctor)
+    return Response(doctor.data)
+
+
+@api_view(['PUT'])
+def update_logged_in_doctor(request):
+    doctor = _get_logged_in_doctor(request)
+    if not doctor:
+        return Response({'message': 'not allowed'}, status=status.HTTP_403_FORBIDDEN)
+
     doctor = DoctorProfileModelSerializer(doctor, data=request.data, partial=True)
     if doctor.is_valid():
         doctor.save()

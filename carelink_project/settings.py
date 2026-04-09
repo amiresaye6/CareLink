42a36c0fad80f8558bc129068e9f0daf905b19fc
Amir Alsayed <amyralsyd367@gmail.com>
2026-04-10 00:27:34 +0200

    fix: update settings, add admin usrls to global urls

--- carelink_project/settings.py
+++ carelink_project/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
-        'PASSWORD': '',
+        'PASSWORD':  env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
         'PORT': env('DB_PORT'),
     }
@@ -124,7 +124,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 
--- carelink_project/urls.py
+++ carelink_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/admin/', include('dashboard.api.urls')),
     path('api/accounts/', include('accounts.apis.urls')),
 ]

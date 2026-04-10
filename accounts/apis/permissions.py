03109a2076b384e3f0f66a71d8e46b8d90df4a3d
Azzazy6269 <mohamadelazzazy@gmail.com>
2026-04-10 15:49:48 +0200

    refactor: replace role-based permissions with Django group-based permissions

--- accounts/apis/permissions.py
+++ accounts/apis/permissions.py
@@ -2,20 +2,24 @@ from rest_framework.permissions import BasePermission
 
 class IsPatient(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.role=="PATIENT")
+        #return bool(request.user and request.user.is_authenticated and request.user.role=="PATIENT")
+        return bool(request.user and request.user.groups.filter(name='Patients').exists())
     
 
 class IsDoctor(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.role=="DOCTOR")
+        #return bool(request.user and request.user.is_authenticated and request.user.role=="DOCTOR")
+        return bool(request.user and request.user.groups.filter(name='Doctors').exists())
     
 
 class IsReceptionist(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.role=="RECEPTIONIST")
+        #return bool(request.user and request.user.is_authenticated and request.user.role=="RECEPTIONIST")
+        return bool(request.user and request.user.groups.filter(name='Receptionists').exists())
     
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.role=="ADMIN")
+        #return bool(request.user and request.user.is_authenticated and request.user.role=="ADMIN")
+        return bool(request.user and request.user.groups.filter(name='Admins').exists())
     

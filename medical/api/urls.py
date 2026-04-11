4295fec22ecb74f82c1ab27bc100ed96f7445121
donia <doniaamohamed498@gmail.com>
2026-04-11 11:05:01 +0200

    add permition and rules

--- medical/api/urls.py
+++ medical/api/urls.py
@@ -2,13 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/consultations/', views.consultation_list, name='api.consultation.list'),
-    path('api/consultations/<int:id>', views.consultation_detail, name='api.consultation.detail'),
-
-    path('api/prescriptions/', views.prescription_list, name='api.prescription.list'),
-    path('api/prescriptions/<int:id>', views.prescription_detail, name='api.prescription.detail'),
-
-    path('api/tests/', views.test_list, name='api.test.list'),
-    path('api/tests/<int:id>', views.test_detail, name='api.test.detail'),
-    path('api/summary/<int:id>', views.patient_consultation_summary, name='api.consultation.summary'),
+    path('doctor/queue/', views.doctor_queue, name='doctor.queue'),
+    path('doctor/consultation/<int:appointment_id>/save/', views.save_consultation, name='doctor.consultation.save'),
+    path('doctor/consultation/<int:appointment_id>/', views.get_consultation, name='doctor.consultation'),
+    path('patient/history/<int:appointment_id>/', views.patient_consultation_summary, name='patient.summary'),
 ]
--- medical/api/views.py
+++ medical/api/views.py
@@ -1,3 +1,4 @@
+from time import timezone
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from rest_framework.decorators import api_view, permission_classes
@@ -6,104 +7,107 @@ from rest_framework import status ,permissions
 from medical.api.serializers import ConsultationRecordSerializer, PrescriptionItemSerializer, TestRequestSerializer
 from medical.models import ConsultationRecord, PrescriptionItem, TestRequest
 from appointments.models import Appointment
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser ,IsAuthenticatedOrReadOnly ,BasePermission
-# from rest_framework.permissions import SAFE_METHODS
-from accounts.apis  import IsDoctor, IsPatient, IsReceptionist, IsAdmin ,IsDoctorOrReadOnly, IsPatientOrReadOnly, IsReceptionistOrReadOnly, IsAdminOrReadOnly ,IsDoctorOrAdmin, IsPatientOrAdmin, IsReceptionistOrAdmin ,IsDoctorOrReceptionist, IsPatientOrReceptionist, IsAdminOrReceptionist ,IsDoctorOrPatient, IsDoctorOrReceptionistOrAdmin, IsPatientOrReceptionistOrAdmin , IsDoctorOrPatientOrAdmin ,IsDoctorOrPatientOrReceptionist ,IsDoctorOrReceptionistOrAdmin , IsPatientOrReceptionistOrAdmin
-
+from rest_framework.permissions import AllowAny, IsAuthenticated,BasePermission
+from accounts.apis.permissions  import IsDoctor, IsPatient 
+from django.db import transaction
+from django.utils import timezone
+from appointments.api.serializers import BookAppointmentSerializer
 # Create your views here.
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def save_consultation(request, appointment_id):
+    appointment = get_object_or_404(Appointment, pk=appointment_id)
+    if appointment.doctor != request.user.doctor_profile:
+        return Response(
+            {'error': 'You are not the doctor for this appointment'},
+            status=status.HTTP_403_FORBIDDEN
+        )
+
+    if appointment.status != 'CHECKED_IN':
+        return Response(
+            {'error': 'Consultation can only be filled for CHECKED_IN appointments'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    if ConsultationRecord.objects.filter(appointment=appointment).exists():
+        return Response(
+            {'error': 'Consultation already exists for this appointment'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    with transaction.atomic():
+        consultation = ConsultationRecord.objects.create(
+            appointment=appointment,
+            diagnosis=request.data.get('diagnosis', ''),
+            clinical_notes=request.data.get('clinical_notes', '')
+        )
+
+        for rx in request.data.get('prescriptions', []):
+            PrescriptionItem.objects.create(
+                consultation=consultation,
+                drug_name=rx.get('drug_name', ''),
+                dose=rx.get('dose', ''),
+                duration_days=rx.get('duration_days', 1)
+            )
+
+        for test in request.data.get('tests', []):
+            TestRequest.objects.create(
+                consultation=consultation,
+                test_name=test.get('test_name', '')
+            )
+
+        appointment.status = 'COMPLETED'
+        appointment.save()
+
+    return Response(
+        ConsultationRecordSerializer(consultation).data,
+        status=status.HTTP_201_CREATED
+    )
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def patient_consultation_summary(request, appointment_id):
+    appointment = get_object_or_404(Appointment, pk=appointment_id)
+
+    if appointment.patient != request.user.patient_profile:
+        return Response(
+            {'error': 'You are not authorized to view this consultation'},
+            status=status.HTTP_403_FORBIDDEN
+        )
+
+    if appointment.status != 'COMPLETED':
+        return Response(
+            {'error': 'Consultation not completed yet'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    consultation = get_object_or_404(ConsultationRecord, appointment=appointment)
+    return Response(ConsultationRecordSerializer(consultation).data)
+
+
+@api_view(['GET'])
 @permission_classes([IsDoctor])
-def consultation_list(request):
-    if request.method == 'POST':
-        serializer = ConsultationRecordSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    consultations = ConsultationRecord.objects.all()
-    serializer = ConsultationRecordSerializer(consultations, many=True)
+def get_consultation(request, appointment_id):
+    appointment = get_object_or_404(Appointment, pk=appointment_id)
+    if appointment.doctor != request.user.doctor_profile:
+        return Response(
+            {'error': 'You are not the doctor for this appointment'},
+            status=status.HTTP_403_FORBIDDEN
+        )
+
+    consultation = get_object_or_404(ConsultationRecord, appointment=appointment)
+    return Response(ConsultationRecordSerializer(consultation).data)
+
+@api_view(['GET'])
+@permission_classes([IsDoctor])
+def doctor_queue(request):
+    today = timezone.now().date()
+    appointments = Appointment.objects.filter(
+        doctor=request.user.doctor_profile,
+        scheduled_datetime__date=today,
+        status='CHECKED_IN'
+    ).order_by('check_in_time')
+    
+    from appointments.api.serializers import AppointmentSerializer
+    serializer = AppointmentSerializer(appointments, many=True)
     return Response(serializer.data)
-@api_view(['GET', 'PUT', 'DELETE'])
-@permission_classes([IsDoctor])
-def consultation_detail(request, id):
-    consultation = get_object_or_404(ConsultationRecord, pk=id)
-
-    if request.method == 'GET':
-        return Response(ConsultationRecordSerializer(consultation).data)
-
-    elif request.method == 'PUT':
-        serializer = ConsultationRecordSerializer(consultation, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        consultation.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-@api_view(['GET', 'POST'])
-@permission_classes([IsDoctor])
-def prescription_list(request):
-    if request.method == 'POST':
-        serializer = PrescriptionItemSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    prescriptions = PrescriptionItem.objects.all()
-    serializer = PrescriptionItemSerializer(prescriptions, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-@permission_classes([IsDoctor])
-def prescription_detail(request, id):
-    prescription = get_object_or_404(PrescriptionItem, pk=id)
-
-    if request.method == 'GET':
-        return Response(PrescriptionItemSerializer(prescription).data)
-
-    elif request.method == 'PUT':
-        serializer = PrescriptionItemSerializer(prescription, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        prescription.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-@api_view(['GET', 'POST'])
-@permission_classes([IsDoctor])
-def test_list(request):
-    if request.method == 'POST':
-        serializer = TestRequestSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    tests = TestRequest.objects.all()
-    serializer = TestRequestSerializer(tests, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-@permission_classes([IsPatient])
-def test_detail(request, id):
-    test = get_object_or_404(TestRequest, pk=id)
-
-    if request.method == 'GET':
-        return Response(TestRequestSerializer(test).data)
-
-    elif request.method == 'PUT':
-        serializer = TestRequestSerializer(test, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        test.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)

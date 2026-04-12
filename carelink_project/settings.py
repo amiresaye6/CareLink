dc5f13bf1b5b8c7c1e2e8b4631741a219e1e3171
Azzazy6269 <mohamadelazzazy@gmail.com>
2026-04-12 23:58:27 +0200

    fixbug/makemigrations

--- carelink_project/settings.py
+++ carelink_project/settings.py
@@ -119,7 +119,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
-        'PASSWORD':  env('DB_PASSWORD'),
+        'PASSWORD':  '',
         'HOST': env('DB_HOST'),
         'PORT': env('DB_PORT'),
     }
